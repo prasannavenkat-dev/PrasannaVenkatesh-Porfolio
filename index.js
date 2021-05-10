139a969571b20136ad6f7cb8b39ca0033588c220
unknown <prasannavenkat1998@yahoo.com>
2021-05-10 22:01:38 +0530

    third commit

--- index.js
+++ index.js
@@ -4,27 +4,41 @@ let form=document.getElementById('contactID')
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
-    alert('hi')
  //1
     let mail = new FormData(form);
 //2
-console.log('hiihih')
+
     sendMail(mail)
 
 })
 
 
-async function sendMail(mail){
+// async function sendMail(mail){
 
 
-let res1 = await fetch('https://prasannavenkatesh-portfolio.herokuapp.com//message',{
-    method:"post",
-    body:mail
-})
-let res2 = await res1.json()
-console.log(res2);
-return res2
-}
+// let res1 = await fetch('http://localhost:5000/message',{
+//     method:"post",
+//     body:mail
+// })
+// let res2 = await res1.text()
+// console.log(res2);
+// return res2
+// }
+
+
+const sendMail = (mail) => {
+    //1.
+    fetch("https://prasannavenkatesh-porfolio.herokuapp.com//message", {
+      method: "post", //2.
+      body: mail, //3.
+  
+    }).then((response) => {
+       
+      return response.json();
+    });
+  };
+
+
 
 
 
